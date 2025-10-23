9d87905ba56f2db7cdef5072e6355b0006090e6b
Andrew <trotsiuk.andriy@lll.kpi.ua>
2025-10-24 01:50:52 +0300

    again

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -2,12 +2,12 @@
 import { useState } from "react";
 import css from "./NotesPage.module.css"
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
-import { fetchNotes } from "../../lib/api";
-import SearchBox from "../../components/SearchBox/SearchBox";
-import NoteList from "../../components/NoteList/NoteList";
-import Modal from "../../components/Modal/Modal";
-import NoteForm from "../../components/NoteForm/NoteForm";
-import Pagination from "../../components/Pagination/Pagination";
+import { fetchNotes } from "../../../../lib/api";
+import SearchBox from "../../../../components/SearchBox/SearchBox";
+import NoteList from "../../../../components/NoteList/NoteList";
+import Modal from "../../../../components/Modal/Modal";
+import NoteForm from "../../../../components/NoteForm/NoteForm";
+import Pagination from "../../../../components/Pagination/Pagination";
 import { useDebouncedCallback } from "use-debounce";
 import { useParams, useRouter } from "next/navigation";
 
